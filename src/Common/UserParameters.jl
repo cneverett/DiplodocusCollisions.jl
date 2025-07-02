e2e70e344d68b45988701d57914ca8eae267f4d3
Christopher Everett <cn.everett@outlook.com>
2025-07-02 16:10:19 +0100

    + fix?

--- src/Common/UserParameters.jl
+++ src/Common/UserParameters.jl
@@ -2,7 +2,7 @@ function UserBinaryParameters()
 
     name1::String = eval(Symbol("name1"))
 
-    name1::String = getfield(Main,Symbol("name1"))
+    #name1::String = getfield(Main,Symbol("name1"))
     name2::String = getfield(Main,Symbol("name2"))
     name3::String = getfield(Main,Symbol("name3"))
     name4::String = getfield(Main,Symbol("name4"))
