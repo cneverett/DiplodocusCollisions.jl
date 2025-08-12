384d3c65cfe0d963c8b46a037bde6bf74d5076aa
Christopher Everett <cn.everett@outlook.com>
2025-08-12 10:45:57 +0100

    fix

--- src/Binary/BinaryInteractionIntegration.jl
+++ src/Binary/BinaryInteractionIntegration.jl
@@ -219,6 +219,8 @@ function BinaryInteractionIntegration(Setup::Tuple{Tuple{String,String,String,St
 
         write(f,"CorrectedGainMatrix3",CorrectedGainMatrix3)
         write(f,"CorrectedGainMatrix4",CorrectedGainMatrix4)
+        write(f,"CorrectedLossMatrix3",CorrectedLossMatrix3)
+        write(f,"CorrectedLossMatrix4",CorrectedLossMatrix4)
         close(f)
 
     # ===================================== #
