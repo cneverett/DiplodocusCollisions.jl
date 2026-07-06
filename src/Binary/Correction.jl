cc156d3096fdbab88ce92780b9204e2272fba312
Christopher N. Everett <cn.everett@outlook.com>
2026-07-06 12:32:16 +0100

    bug fix

--- src/Binary/Correction.jl
+++ src/Binary/Correction.jl
@@ -633,9 +633,6 @@ function GainCorrectionChunk!(Parameters::Tuple{String, String, String, String, 
 
     for h2 in axes(GainMatrix3,7), u2 in axes(GainMatrix3,6), h1 in axes(GainMatrix3,5), u1 in axes(GainMatrix3,4)
 
-        high_3_range::Int64 = 1
-        high_4_range::Int64 = 1
-
         # generate filtered gain matrices to smooth out spectrum
         GainMatrix3Filtered = @view(GainMatrix3[:,:,:,u1,h1,u2,h2])
         GainMatrix4Filtered = @view(GainMatrix4[:,:,:,u1,h1,u2,h2])
@@ -657,14 +654,14 @@ function GainCorrectionChunk!(Parameters::Tuple{String, String, String, String, 
         partialsortperm!(high_inds3,tmpsortvec3, 1:size3; rev=true)
         #cart_inds3 .= CartesianIndices(GainMatrix3Filtered)[high_inds3]
         ci3 = CartesianIndices(GainMatrix3Filtered)
-        @inbounds for i in 1:high_3_range
+        @inbounds for i in eachindex(high_inds3)
             cart_inds3[i] = ci3[high_inds3[i]]
         end
 
         partialsortperm!(high_inds4,tmpsortvec4, 1:size4; rev=true)
         #cart_inds4 .= CartesianIndices(GainMatrix4Filtered)[high_inds4]
         ci4 = CartesianIndices(GainMatrix4Filtered)
-        @inbounds for i in 1:high_4_range
+        @inbounds for i in eachindex(high_inds4)
             cart_inds4[i] = ci4[high_inds4[i]]
         end
 
@@ -710,6 +707,9 @@ function GainCorrectionChunk!(Parameters::Tuple{String, String, String, String, 
 
         p3_offset = length(axes(GainMatrix3,1))
         p4_offset = length(axes(GainMatrix4,1))
+
+        high_3_range::Int64 = 1
+        high_4_range::Int64 = 1
 
         #=if E1_d[p1] > 1e0*E2_d[p2] && m1 > m2
             p1Big = true
